4b457c44cb2303f7c8a5df4f92cd7fc4b13223d9
ChenYing Kuo <evshary@gmail.com>
2022-12-31 14:49:33 +0800

    Refactor how to get title.

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Parse URL: {}", url);
         let resp = reqwest::get(url).await?;
         let text = resp.text().await?;
-        //println!("{}", text);
         let document = Html::parse_document(&text);
+        // Get the title
         let selector = Selector::parse("h1").unwrap();
-        for title in document.select(&selector) {
-            let mut title = title.inner_html();
-            title.retain(|c| !"\t\r\n".contains(c));
-            let splits = title.trim().split('|').collect::<Vec<&str>>();
-            let title = String::from(splits[1]) + " - " + splits[0];
-            println!("{}", title);
-        }
+        let mut title = document
+            .select(&selector)
+            .nth(0) // Get first element
+            .unwrap()
+            .inner_html();
+        title.retain(|c| !"\t\r\n".contains(c));
+        let splits = title.trim().split('|').collect::<Vec<&str>>();
+        let title = String::from(splits[1]) + " - " + splits[0];
+        println!("{}", title);
     }
     Ok(())
 }
